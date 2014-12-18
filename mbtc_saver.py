9d0f1aa9bb5926d5726cb3666102d45b2e93c8dd
Pedro Grojsgold <pedro@grojsgold.com>
2014-12-18 00:14:57 -0200

    Add trades logger.

--- mbtc_saver.py
+++ mbtc_saver.py
@@ -4,22 +4,34 @@ import time
 from pymongo import MongoClient
 
 CACHE_TIME = 61
+QUOTES_COLLECTION = 'quotes'
+TRADES_COLLECTION = 'trades'
+COIN = 'btc'
+
+
+def process_book(db):
+    quotes = db[QUOTES_COLLECTION]
+    book = mbtcapi.getOrderBook(COIN)
+    book['ts'] = datetime.datetime.utcnow()
+    quotes.insert(book)
+
+
+def process_trades(db):
+    trades_col = db[TRADES_COLLECTION]
+    trades = mbtcapi.getTrades(COIN)
+    for trade in trades:
+        trades_col.update({'tid':trade['tid']}, trade, upsert=True)
+
 
 def main():
     client = MongoClient()
     db = client['MBTC']
-    quotes = db['quotes']
-
-    last_book = {}
 
     while True:
-        book = mbtcapi.getOrderBook('btc')
-        last_book = book
-        print book
-        book['ts'] = datetime.datetime.utcnow()
-        quotes.insert(book)
-
+        process_book(db)
+        process_trades(db)
         time.sleep(CACHE_TIME)
+
 
 if __name__ == "__main__":
     main()
